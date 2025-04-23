b9d1c8bcba261cff9409962b1435d22a54cf80fa
Unique-leyi <aywizolu@gmail.com>
2025-04-23 13:00:12 +0100

    changes: modify changes to orders and backend

--- Backend/app.py
+++ Backend/app.py
@@ -29,6 +29,7 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'shopping_cartdb')
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Configure file uploads
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/uploads')
