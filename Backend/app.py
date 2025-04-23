018df85da4e180196b3f967a6ecbf21418d1af39
Unique-leyi <aywizolu@gmail.com>
2025-04-23 13:01:09 +0100

    changes: modify changes to orders and backend

--- Backend/app.py
+++ Backend/app.py
@@ -2359,4 +2359,4 @@ def handle_delivery_options_request(delivery_id=None, order_number=None, trackin
 #     app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
